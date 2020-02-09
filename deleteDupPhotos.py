b3bb817373fcceadbc35b0a43358c762ab5086cb
Quinn Adams <qadams@unomaha.edu>
2020-02-09 17:07:49 -0600

    initial commit

---
+++ deleteDupPhotos.py
@@ -0,0 +1,13 @@
+import os
+import fnmatch
+import sys
+
+searchRootDir = sys.argv[1]
+filenamePattern = '[a-zA-Z0-9]*([0-9]+)*.[a-zA-Z]*'
+#filenamePattern = '[a-zA-Z0-9_]*([0-9]+)*[a-zA-Z0-9_]*'
+
+files = os.listdir(searchRootDir)
+duplicateFiles = fnmatch.filter(files, filenamePattern)
+
+print(duplicateFile) for duplicateFile in duplicateFiles
+#os.remove(duplicateFile) for duplicateFile in duplicateFiles
